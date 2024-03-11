cf733de15c9ef989b07f1bd0c3afd38f9a1d7812
parill <157394749+parill@users.noreply.github.com>
2024-03-11 23:53:08 +0700

    SWEETALERT HOMEUSERS
    INI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- Mirrored from wpkixx.com/html/socimo/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 01 Mar 2024 06:20:33 GMT -->
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,6 +12,11 @@
 <link rel="stylesheet" href="socimo/css/style.css">
 <link rel="stylesheet" href="socimo/css/color.css">
 <link rel="stylesheet" href="socimo/css/responsive.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
@@ -1498,49 +1502,38 @@ i think that some how, we learn who we really are and then live with that decisi
     });
 </script>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    @if (Session::has('success'))
+    toastr.success("{{ Session::get('success') }}")
+    @endif
+    function swalpFunction() {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                console.log("Data dihapus");
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                });
+            }
+        });
+    }
+</script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-<script src="js/main.min.js" type="101cca6ef11d27050cf841ef-text/javascript"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="js/main.min.js" type="text/javascript"></script>
 <script src="js/date-time.js" type="text/javascript"></script>
 <script src="js/script.js" type="b792af529d8fc78a3581caf5-text/javascript"></script>
-{{-- <script src="../../cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="b792af529d8fc78a3581caf5-|49" defer></script><script>(function(){var js = "window['__CF$cv$params']={r:'85d70a9af83e836b',t:'MTcwOTI3NDAyOS42NjIwMDA='};_cpo=document.createElement('script');_cpo.nonce='',_cpo.src='../../cdn-cgi/challenge-platform/h/g/scripts/jsd/a0d8959cb7d0/main.js',document.getElementsByTagName('head')[0].appendChild(_cpo);";var _0xh = document.createElement('iframe');_0xh.height = 1;_0xh.width = 1;_0xh.style.position = 'absolute';_0xh.style.top = 0;_0xh.style.left = 0;_0xh.style.border = 'none';_0xh.style.visibility = 'hidden';document.body.appendChild(_0xh);function handler() {var _0xi = _0xh.contentDocument || _0xh.contentWindow.document;if (_0xi) {var _0xj = _0xi.createElement('script');_0xj.innerHTML = js;_0xi.getElementsByTagName('head')[0].appendChild(_0xj);}}if (document.readyState !== 'loading') {handler();} else if (window.addEventListener) {document.addEventListener('DOMContentLoaded', handler);} else {var prev = document.onreadystatechange || function () {};document.onreadystatechange = function (e) {prev(e);if (document.readyState !== 'loading') {document.onreadystatechange = prev;handler();}};}})();</script></body> --}}
-{{--
-<script>
-    (function(){
-        var js = "window['__CF$cv$params']={r:'85d70a9af83e836b',t:'MTcwOTI3NDAyOS42NjIwMDA='};_cpo=document.createElement('script');_cpo.nonce='',_cpo.src='../../cdn-cgi/challenge-platform/h/g/scripts/jsd/a0d8959cb7d0/main.js',document.getElementsByTagName('head')[0].appendChild(_cpo);";
-        var _0xh = document.createElement('iframe');
-        _0xh.height = 1;
-        _0xh.width = 1;
-        _0xh.style.position = 'absolute';
-        _0xh.style.top = 0;
-        _0xh.style.left = 0;
-        _0xh.style.border = 'none';
-        _0xh.style.visibility = 'hidden';
-        document.body.appendChild(_0xh);
-        function handler() {
-            var _0xi = _0xh.contentDocument || _0xh.contentWindow.document;
-            if (_0xi) {
-                var _0xj = _0xi.createElement('script');
-                _0xj.innerHTML = js;
-                _0xi.getElementsByTagName('head')[0].appendChild(_0xj);
-            }
-        }
-        if (document.readyState !== 'loading') {
-            handler();
-        } else if (window.addEventListener) {
-            document.addEventListener('DOMContentLoaded', handler);
-        } else {
-            var prev = document.onreadystatechange || function () {};
-            document.onreadystatechange = function (e) {
-                prev(e);
-                if (document.readyState !== 'loading') {
-                    document.onreadystatechange = prev;
-                    handler();
-                }
-            };
-        }
-    })();
-</script> --}}
+
 </body>
 
 
