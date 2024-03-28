1063025b4df531838e6b6b933839d6edb3e1097b
Deonnx <152576051+Deonnx@users.noreply.github.com>
2024-03-28 09:44:33 +0700

    notif indikator

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -93,6 +93,20 @@
             height: 100%;
             object-fit: cover;
         }
+        .notification-count {
+            position: absolute;
+            top: 6px;
+            right: -1px;
+            background-color: red;
+            color: white;
+            border-radius: 50%;
+            padding: 2px 4px;
+            font-size: 11px;
+            width: 15px;
+            height: 15px;
+            line-height: 11px;
+            text-align: center;
+}
     </style>
 </head>
 
@@ -159,19 +173,17 @@
                         @endif
                     </li>
                     <li>
-                        <a class="mesg-notif" href="#" title="Messages" data-toggle="modal"
-                            data-target="#myModal">
+                        <a class="mesg-notif" href="#" title="Messages" data-toggle="modal" data-target="#myModal">
                             <i>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell">
                                     <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
                                     <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
                                 </svg>
                             </i>
+                            <span class="notification-count">1</span> <!-- notif -->
                         </a>
-                        <span></span>
                     </li>
+
                     <li>
                         <a href="#" title>
                             <i>
